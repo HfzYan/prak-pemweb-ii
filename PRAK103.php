2e8cf9e385c78d7b1d41182aa264edea5bb52453
Muhammad Hafizh <90849858+HfzYan@users.noreply.github.com>
2022-03-14 23:01:57 +0800

    Update PRAK103.php

--- PRAK103.php
+++ PRAK103.php
@@ -7,9 +7,9 @@
     $r = 4 / 5 * $c;
     $f = (9 / 5 * $c) + 32;
     $k = $c + 273.15;
-    echo "Fahrenheit (F) = " . $f . "<br>";
-    echo "Reamur (R) = " . $r . "<br>";
-    echo "Kelvin (K) = " . $k . "<br>";
+    echo "Fahrenheit (F) = " . round($f, 4) . "<br>";
+    echo "Reamur (R) = " . round($r, 4) . "<br>";
+    echo "Kelvin (K) = " . round($k, 4) . "<br>";
     ?>
 </body>
 

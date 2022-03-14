d5eeb2c536cce065cf5e578de277b0bfbaad5c8d
Muhammad Hafizh <90849858+HfzYan@users.noreply.github.com>
2022-03-14 11:07:36 +0800

    Add files via upload

---
+++ PRAK103.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $c = 37.841;
+    $r = 4 / 5 * $c;
+    $f = (9 / 5 * $c) + 32;
+    $k = $c + 273.15;
+    echo "Fahrenheit (F) = " . $f . "<br>";
+    echo "Reamur (R) = " . $r . "<br>";
+    echo "Kelvin (K) = " . $k . "<br>";
+    ?>
+</body>
+
+</html>

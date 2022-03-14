568ca883bdd7b3ad177419c8fe4056884ca0d2c7
Muhammad Hafizh <90849858+HfzYan@users.noreply.github.com>
2022-03-14 10:06:43 +0800

    Add files via upload

---
+++ PRAK101.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?= "Hello World in PHP" ?><br>
+    <?= "Nama Praktikan : Muhammad Hafizh Yanuardi" ?><br>
+    <?= "NIM : 1910817210023" ?><br>
+</body>
+
+</html>
